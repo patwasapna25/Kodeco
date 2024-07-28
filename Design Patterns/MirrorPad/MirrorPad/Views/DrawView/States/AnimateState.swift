972a55a44bad7e51118ec90272a5caff25d3681f
Sapna Patwa <spatwa@aemac019.local>
2024-07-28 23:35:32 +0530

    State Design Pattern

--- Design Patterns/MirrorPad/MirrorPad/Views/DrawView/States/AnimateState.swift
+++ Design Patterns/MirrorPad/MirrorPad/Views/DrawView/States/AnimateState.swift
@@ -1,15 +1,15 @@
-/// Copyright (c) 2018 Razeware LLC
-///
+/// Copyright (c) 2024 Razeware LLC
+/// 
 /// Permission is hereby granted, free of charge, to any person obtaining a copy
 /// of this software and associated documentation files (the "Software"), to deal
 /// in the Software without restriction, including without limitation the rights
 /// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 /// copies of the Software, and to permit persons to whom the Software is
 /// furnished to do so, subject to the following conditions:
-///
+/// 
 /// The above copyright notice and this permission notice shall be included in
 /// all copies or substantial portions of the Software.
-///
+/// 
 /// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
 /// distribute, sublicense, create a derivative work, and/or sell copies of the
 /// Software in any work that is designed, intended, or marketed for pedagogical or
@@ -17,7 +17,7 @@
 /// or information technology.  Permission for such use, copying, modification,
 /// merger, publication, distribution, sublicensing, creation of derivative works,
 /// or sale is expressly withheld.
-///
+/// 
 /// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 /// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 /// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -28,59 +28,27 @@
 
 import UIKit
 
-public class DrawView: UIView {
-
-  // MARK: - Instance Properties
-  public var lineColor: UIColor = .black
-  public var lineWidth: CGFloat = 5.0
-  public var lines: [LineShape] = []
-
-  @IBInspectable public var scaleX: CGFloat = 1 {
-    didSet { applyTransform() }
-  }
-  @IBInspectable public var scaleY: CGFloat = 1 {
-    didSet { applyTransform() }
-  }
-  private func applyTransform() {
-    layer.sublayerTransform = CATransform3DMakeScale(scaleX, scaleY, 1)
-  }
-
-  // MARK: - UIResponder
-  public override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-    guard let point = touches.first?.location(in: self) else { return }
-    let line = LineShape(color: lineColor, width: lineWidth, startPoint: point)
-    lines.append(line)
-    layer.addSublayer(line)
-  }
-
-  public override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-    guard let point = touches.first?.location(in: self),
-      let currentLine = lines.last else { return }
-    currentLine.addPoint(point)
-  }
-
-  // MARK: - Actions
-  public func animate() {
-    guard let sublayers = layer.sublayers, sublayers.count > 0 else { return }
+public class AnimateState: DrawViewState {
+  public override func animate() {
+    guard let sublayers = drawView.layer.sublayers,
+      sublayers.count > 0 else {
+        transitionToState(matching: AcceptInputState.identifier)
+        return
+    }
     sublayers.forEach { $0.removeAllAnimations() }
-    UIView.beginAnimations(nil, context: nil)
-    CATransaction.begin()
-    setSublayersStrokeEnd(to: 0.0)
-    animateStrokeEnds(of: sublayers, at: 0)
-    CATransaction.commit()
-    UIView.commitAnimations()
+    UIView.animate(withDuration: 0.3) {
+      CATransaction.begin()
+      CATransaction.setCompletionBlock { [weak self] in
+        self?.transitionToState(matching: AcceptInputState.identifier)
+      }
+      self.setSublayersStrokeEnd(to: 0.0)
+      self.animateStrokeEnds(of: sublayers, at: 0)
+      CATransaction.commit()
+    }
   }
   
-  public func copyingLines(form source: DrawView) {
-    layer.sublayers?.removeAll()
-    lines = source.lines.deepCopy()
-    lines.forEach({
-      layer.addSublayer($0)
-    })
-  }
-
   private func setSublayersStrokeEnd(to value: CGFloat) {
-    layer.sublayers?.forEach {
+    drawView.layer.sublayers?.forEach {
       guard let shapeLayer = $0 as? CAShapeLayer else { return }
       shapeLayer.strokeEnd = 0.0
       let animation = CABasicAnimation(keyPath: "strokeEnd")
@@ -90,7 +58,7 @@ public class DrawView: UIView {
       shapeLayer.add(animation, forKey: nil)
     }
   }
-
+  
   private func animateStrokeEnds(of layers: [CALayer], at index: Int) {
     guard index < layers.count else { return }
     let currentLayer = layers[index]
@@ -109,10 +77,5 @@ public class DrawView: UIView {
       shapeLayer.add(animation, forKey: nil)
     }
     CATransaction.commit()
-  }
-
-  public func clear() {
-    lines = []
-    layer.sublayers?.removeAll()
   }
 }
